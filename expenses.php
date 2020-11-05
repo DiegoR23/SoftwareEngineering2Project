bf3cdd61aeaad4995aba82b788a1f18df900c4b6
Diegor23 <diego.tamuk@gmail.com>
2020-11-05 13:51:18 -0600

    updated expense.php

--- expenses.php
+++ expenses.php
@@ -8,6 +8,9 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
 
  <?php
 
+  // user variable
+  $userID = $_SESSION['user'];
+
   // budget form
    $budgetform =
    '
@@ -32,7 +35,7 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
            <label for="budget"><strong>Budget</strong></label>
            <input class="form-expense-input" type="number" name="budget" placeholder="Budget" required>
          </div>
-         <button class="form-button" type="submit" name="budget" >Add Budget</button>
+         <button class="form-button" type="submit" name="budgetbutton" >Add Budget</button>
        </div>
      </form>
    </div>
@@ -40,10 +43,10 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
 
     // expense form
     $expenseform =
-      '
-      <div class="form-body">
-        <form method="post" class="form-form">
-          <h2>Expense</h2>
+    '
+    <div class="form-body">
+      <form method="post" class="form-form">
+        <h2>Expense</h2>
           <div class="form-container">
             <hr>
             <div class="expense-container">
@@ -64,18 +67,39 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
                 <option value="Other">Other</option>
               </select>
             </div>
-            <button class="form-button" type="submit" name="expense">Add Expense</button>
-          </div>
-        </form>
-      </div>
-      ';
+          <button class="form-button" type="submit" name="expensebutton">Add Expense</button>
+        </div>
+      </form>
+    </div>
+    ';
  ?>
+ <?php
+
+    if(isset($_POST["budgetbutton"], $_POST["category"], $_POST["budget"])
+        && is_string($_POST["category"])
+        && is_numeric($_POST["budget"])
+      ){
+        $category = filter_var(trim($_POST["category"]), FILTER_SANITIZE_STRING);
+        $budget = filter_var(trim($_POST["budget"]), FLITER_SANITIZE_STRING);
+        $sql = "SELECT category from budget where category='" . $category . "' and userID='" . $userID . "' LIMIT 1;";
+        $check = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        if (empty($check)){
+          $sql = "INSERT INTO budget (category, budget, userID) VALUES ('" . $category ."', '" . $budget . "', '" . $userID ."');";
+          $db->exec($sql);
+        }
+        else {
+          $sql = "UPDATE budget SET budget='" . $budget ."' WHERE category='" . $category ."' and userID='" . $userID . "';";
+          $db->exec($sql);
+        }
+
+      }
+
+  ?>
  <!DOCTYPE html>
  <html lang="en" dir="ltr">
    <?php require_once './layouts/head.php'; ?>
    <body>
      <?php require_once './layouts/nav.php'; ?>
-     <<?php $user = $_SESSION['user'] ?>
 
     <h1>Add Or Update Your Category Budgets:</h1>
 
@@ -96,7 +120,6 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
     <div class="button-container">
       <button class="round-button" type="submit" name="addexpenseform">+</button>
     </div>
-
 
     <?php echo '<p> This is the usersID: '.$_SESSION['user'].'</p>'; ?>
    </body>
