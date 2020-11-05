be2006bcde99919e5251043904384c7bda154e41
Diegor23 <diego.tamuk@gmail.com>
2020-11-05 10:51:31 -0600

    updated expense.php

--- expenses.php
+++ expenses.php
@@ -13,28 +13,32 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
      <<?php $user = $_SESSION['user'] ?>
 
     <h1>Add Or Update Your Category Budgets:</h1>
-    <form method="post" class="form-form">
-      <h2>Budget</h2>
-      <div class="form-container">
-        <hr>
-        <div class="container">
-          <label for="category"><strong>Category</strong></label>
-          <select name="category" >
-            <option value="Food & Drink">Food & Drink</option>
-            <option value="Groceries">Groceries</option>
-            <option value="Shopping & Entertainment">Shopping & Entertainment</option>
-            <option value="Home & Utilities">Home & Utilities</option>
-            <option value="Transportation">Transportation</option>
-            <option value="Travel">Travel</option>
-            <option value="Personal">Personal</option>
-            <option value="Other">Other</option>
-          </select>
-          <label for="budget"><strong>Budget</strong></label>
-          <input class="" type="number" name="budget" placeholder="Budget" required>
+
+    <div class="form-body">
+      <form method="post" class="form-form">
+        <h2>Budget</h2>
+        <div class="form-container">
+          <hr>
+          <div class="container">
+            <label for="category"><strong>Category</strong></label>
+            <select name="category">
+              <option disabled selected value> -- select an option -- </option>
+              <option value="Food & Drink">Food & Drink</option>
+              <option value="Groceries">Groceries</option>
+              <option value="Shopping & Entertainment">Shopping & Entertainment</option>
+              <option value="Home & Utilities">Home & Utilities</option>
+              <option value="Transportation">Transportation</option>
+              <option value="Travel">Travel</option>
+              <option value="Personal">Personal</option>
+              <option value="Other">Other</option>
+            </select>
+            <label for="budget"><strong>Budget</strong></label>
+            <input class="" type="number" name="budget" placeholder="Budget" required>
+          </div>
+          <button class="" type="submit" name="budget" >Add Budget</button>
         </div>
-        <button class="" type="submit" name="budget" >Add Budget</button>
-      </div>
-    </form>
+      </form>
+    </div>
 
     <h1>Add Your Expenses:</h1>
 
