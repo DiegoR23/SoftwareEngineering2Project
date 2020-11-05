d9f283ab6d4d83d4bb3133768b6bf1e734a7cff8
Diegor23 <diego.tamuk@gmail.com>
2020-11-05 11:06:10 -0600

    updated expense.php

--- expenses.php
+++ expenses.php
@@ -7,6 +7,36 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
  ?>
 
  <?php
+
+ $budgetform =
+ '
+ <div class="form-body">
+   <form method="post" class="form-form">
+     <h2>Budget</h2>
+     <div class="form-container">
+       <hr>
+       <div class="container">
+         <label for="category"><strong>Category</strong></label>
+         <select name="category">
+           <option disabled selected value> -- select an option -- </option>
+           <option value="Food & Drink">Food & Drink</option>
+           <option value="Groceries">Groceries</option>
+           <option value="Shopping & Entertainment">Shopping & Entertainment</option>
+           <option value="Home & Utilities">Home & Utilities</option>
+           <option value="Transportation">Transportation</option>
+           <option value="Travel">Travel</option>
+           <option value="Personal">Personal</option>
+           <option value="Other">Other</option>
+         </select>
+         <label for="budget"><strong>Budget</strong></label>
+         <input class="" type="number" name="budget" placeholder="Budget" required>
+       </div>
+       <button class="" type="submit" name="budget" >Add Budget</button>
+     </div>
+   </form>
+ </div>
+ ';
+ 
   $expenseform =
     '
     <div class="form-body">
@@ -33,35 +63,6 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
             </select>
           </div>
           <button class="form-expense-button" type="submit" name="expense">Add Expense</button>
-        </div>
-      </form>
-    </div>
-    ';
-
-    $budgetform =
-    '
-    <div class="form-body">
-      <form method="post" class="form-form">
-        <h2>Budget</h2>
-        <div class="form-container">
-          <hr>
-          <div class="container">
-            <label for="category"><strong>Category</strong></label>
-            <select name="category">
-              <option disabled selected value> -- select an option -- </option>
-              <option value="Food & Drink">Food & Drink</option>
-              <option value="Groceries">Groceries</option>
-              <option value="Shopping & Entertainment">Shopping & Entertainment</option>
-              <option value="Home & Utilities">Home & Utilities</option>
-              <option value="Transportation">Transportation</option>
-              <option value="Travel">Travel</option>
-              <option value="Personal">Personal</option>
-              <option value="Other">Other</option>
-            </select>
-            <label for="budget"><strong>Budget</strong></label>
-            <input class="" type="number" name="budget" placeholder="Budget" required>
-          </div>
-          <button class="" type="submit" name="budget" >Add Budget</button>
         </div>
       </form>
     </div>
