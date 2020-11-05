2297c6fecf51739b429ce85a3a6875a3e2c01d95
Diegor23 <diego.tamuk@gmail.com>
2020-11-05 11:04:47 -0600

    updated expense.php

--- expenses.php
+++ expenses.php
@@ -5,43 +5,10 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
   header("Location: index.php");
 }
  ?>
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <?php require_once './layouts/head.php'; ?>
-   <body>
-     <?php require_once './layouts/nav.php'; ?>
-     <<?php $user = $_SESSION['user'] ?>
 
-    <h1>Add Or Update Your Category Budgets:</h1>
-
-    <div class="form-body">
-      <form method="post" class="form-form">
-        <h2>Budget</h2>
-        <div class="form-container">
-          <hr>
-          <div class="container">
-            <label for="category"><strong>Category</strong></label>
-            <select name="category">
-              <option disabled selected value> -- select an option -- </option>
-              <option value="Food & Drink">Food & Drink</option>
-              <option value="Groceries">Groceries</option>
-              <option value="Shopping & Entertainment">Shopping & Entertainment</option>
-              <option value="Home & Utilities">Home & Utilities</option>
-              <option value="Transportation">Transportation</option>
-              <option value="Travel">Travel</option>
-              <option value="Personal">Personal</option>
-              <option value="Other">Other</option>
-            </select>
-            <label for="budget"><strong>Budget</strong></label>
-            <input class="" type="number" name="budget" placeholder="Budget" required>
-          </div>
-          <button class="" type="submit" name="budget" >Add Budget</button>
-        </div>
-      </form>
-    </div>
-
-    <h1>Add Your Expenses:</h1>
-
+ <?php
+  $expenseform =
+    '
     <div class="form-body">
       <form method="post" class="form-form">
         <h2>Expense</h2>
@@ -69,6 +36,51 @@ if(!(isset($_SESSION["is_loged_in"]) && $_SESSION["is_loged_in"] == true)){
         </div>
       </form>
     </div>
+    ';
+
+    $budgetform =
+    '
+    <div class="form-body">
+      <form method="post" class="form-form">
+        <h2>Budget</h2>
+        <div class="form-container">
+          <hr>
+          <div class="container">
+            <label for="category"><strong>Category</strong></label>
+            <select name="category">
+              <option disabled selected value> -- select an option -- </option>
+              <option value="Food & Drink">Food & Drink</option>
+              <option value="Groceries">Groceries</option>
+              <option value="Shopping & Entertainment">Shopping & Entertainment</option>
+              <option value="Home & Utilities">Home & Utilities</option>
+              <option value="Transportation">Transportation</option>
+              <option value="Travel">Travel</option>
+              <option value="Personal">Personal</option>
+              <option value="Other">Other</option>
+            </select>
+            <label for="budget"><strong>Budget</strong></label>
+            <input class="" type="number" name="budget" placeholder="Budget" required>
+          </div>
+          <button class="" type="submit" name="budget" >Add Budget</button>
+        </div>
+      </form>
+    </div>
+    ';
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <?php require_once './layouts/head.php'; ?>
+   <body>
+     <?php require_once './layouts/nav.php'; ?>
+     <<?php $user = $_SESSION['user'] ?>
+
+    <h1>Add Or Update Your Category Budgets:</h1>
+
+    <?php echo $budgetform; ?>
+
+    <h1>Add Your Expenses:</h1>
+
+    <?php echo $expenseform; ?>
 
     <?php echo '<p> This is the usersID: '.$_SESSION['user'].'</p>'; ?>
    </body>
